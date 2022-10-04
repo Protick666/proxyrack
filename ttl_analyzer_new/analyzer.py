e273cd73bc3c3c2f37586485d72fedf2f32012c9
protickbhowmick <protickbhowmick@priyo.com>
2022-10-04 02:30:32 -0400

    init

--- ttl_analyzer_new/analyzer.py
+++ ttl_analyzer_new/analyzer.py
@@ -10,7 +10,7 @@ allowed_ttl = ["1", "5", "15", "30", "60"]
 from collections import defaultdict
 import json
 import pyasn
-from ..asn_org_tools.org_finder import AS2ISP
+from asn_org_tools.org_finder import AS2ISP
 from multiprocessing.dummy import Pool as ThreadPool
 import time
 asndb = pyasn.pyasn('../asn_org_tools/data/ipsan_db.dat')
