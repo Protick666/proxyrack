51f1828d11f0aebfc67e59493922f29e1e8c2117
protickbhowmick <protickbhowmick@priyo.com>
2022-10-25 21:19:35 -0400

    init

--- analyzer.py
+++ analyzer.py
@@ -59,7 +59,7 @@ def analyze_files(files, resolver_to_is_dishonor_vote, flag, ttl_list, reached_b
         for req_id in d:
             try:
                 ttl_1 = d[req_id]["ttl_1"]
-                resolver = d[req_id]['tuple'][0]
+                resolver = d[req_id]['resolver']
                 ttl_2 = d[req_id]["ttl_2"]
                 ip_1 = d[req_id]["ip_1"]
                 ip_2 = d[req_id]["ip_2"]
@@ -101,8 +101,11 @@ def get_resolver_to_dishonor_dict():
 def get_resolver_to_dishonor_dict_v2(str):
     ttl_list = []
 
-    direct_dump_files = get_files_from_dir("cross_check_direct_v21/{}/".format(str))
-    proxy_rack_dump_files = get_files_from_dir("cross_check_v21/{}/".format(str))
+    # direct_dump_files = get_files_from_dir("cross_check_direct_v21/{}/".format(str))
+    # proxy_rack_dump_files = get_files_from_dir("cross_check_v21/{}/".format(str))
+
+    direct_dump_files = get_files_from_dir("data/dishonor_direct.json")
+    proxy_rack_dump_files = get_files_from_dir("data/dishonor_proxy.json")
 
     from collections import defaultdict
     resolver_to_is_dishonor_vote = {}
