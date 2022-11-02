16aa7b4ba3b2f5244a3893a8803a0a5655c63642
protickbhowmick <protickbhowmick@priyo.com>
2022-11-02 00:27:37 -0400

    init

--- tmp.py
+++ tmp.py
@@ -1,85 +1,114 @@
-import asyncio
-from os.path import expanduser
-import socket
-import ssl
-import certifi
-from pathlib import Path
-from check_tls_certs import *
-from cryptography.hazmat.primitives import serialization
-from cryptography import x509
-import redis
+import os
+import json
+from fastavro import writer, reader, parse_schema
 
-def tuple_test():
-        domain_tuples = ('google.com', 'facebook.com', 'youtube.com')
-        domains = list(itertools.chain(
-                domain_definitions_from_filename(None),
-                domain_definitions_from_cli(domain_tuples)))
+target_domains = set()
+res = {}
 
-        certs = get_domain_certs(domains)
+CDNs = ['Akamai',
+        'Akadns',
+        'Bitgravity',
+        'Cachefly',
+        'CDN77',
+        'CDNetworks',
+        'CDNify',
+        'ChinaCache',
+        'ChinaNetCenter',
+        'EdgeCast',
+        'Fastly',
+        'Highwinds',
+        'Internap',
+        'KeyCDN',
+        'google',
+        'Level3',
+        'Limelight',
+        'MaxCDN',
+        'NetDNA',
+        'Telefónica',
+        'XCDN',
+        'CloudFlare',
+        'Jetpack',
+        'Rackspace',
+        'CloudLayer',
+        'CloudCache',
+        'TinyCDN',
+        'Incapsula',
+        'jsDelivr',
+        'EdgeCast',
+        'CDNsun',
+        'Limelight',
+        'Azure',
+        'CDNlio',
+        'SoftLayer',
+        'ITWorks',
+        'CloudOY',
+        'Octoshape',
+        'Hibernia',
+        'WebMobi',
+        'CDNvideo',
+        'zerocdn',
+        "cdn",
+        "alibaba",
+        "netlify",
+        "wixdns"
+        ]
 
-        a = certs['google.com'][0].to_cryptography()
-        pem = a.public_bytes(encoding=serialization.Encoding.PEM)
-        cert = pem.decode('utf8')
+CDN_HINTS = ["Akam", "tiny", 'CDN', 'Cloudfront']
 
-
-
-# https://github.com/fschulze/check-tls-certs/blob/main/check_tls_certs.py
-
-def get_certs_akid(cert):
-    a = 1
-    for extension in cert.extensions:
-        oid_str = extension.oid.dotted_string
-        if oid_str == '2.5.29.35':
-            return extension.value.key_identifier.hex()
-    return "-"
-
-def get_certs_ocsp_url(cert):
-    a = 1
-    for extension in cert.extensions:
-        oid_str = extension.oid.dotted_string
-        if oid_str == '1.3.6.1.5.5.7.1.1':
-            for sub_extension in extension.value._descriptions:
-                if sub_extension.access_method.dotted_string == '1.3.6.1.5.5.7.48.1':
-                    return sub_extension.access_location.value
-    return "-"
-
-async def tcp_echo_client(message):
-    # We need a fully qualified domain name for the server.
-    host = "facebook.com"
-    # We need an SSL context.
-    path_to_ca_certs = Path(certifi.where())
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    context.load_verify_locations(
-        path_to_ca_certs
-    )
-
-    context.verify_mode = ssl.CERT_REQUIRED
-    # context.check_hostname = True
-
-    reader, writer = await asyncio.open_connection(
-        host, # use the real host name
-        443,
-        ssl=context # pass in the context.
-    )
-
-    # We will get a certificate from the server.
-    peercert = writer.get_extra_info('peercert')
-    print(f'Peer cert: {peercert!r}')
-    decoded_cert = x509.load_der_x509_certificate(peercert)
-    akid = get_certs_akid(decoded_cert)
-    serial = decoded_cert.serial_number
-    ocsp_url = get_certs_ocsp_url(decoded_cert)
-    a = 1
+multicdns = [
+    "cedexis",
+    "Mlytics",
+    "metacdn",
+    "atanar",
+]
 
 
-    redis_host = "pharah.cs.vt.edu"
 
-    r = redis.Redis(host=redis_host, port=6379, db=0,
-                    password="certificatesarealwaysmisissued")
+def json_dump(d, fn):
+    json.dump(d, open(fn, 'w'), default=str, indent=4)
 
-    ca_cert = fix_cert_indentation(r.get("ocsp:akid:" + akid.upper()).decode())
+def proc_f(f):
+    global target_domains
+    global res
 
-    a = 1
-    writer.close()
+    cnt = 0
+    with open(os.path.join('/tmp', f), 'rb') as fo:
+        for domain in reader(fo):
+            cnt += 1
+            if cnt % 10000 == 0:
+                print(cnt)
+            if domain['cname_name'] is not None and \
+                    domain['query_type'] == "A" and \
+                    domain['response_type'] == "CNAME":
 
-asyncio.run(tcp_echo_client('Hello World!'))
+                for cdn in CDNs + CDN_HINTS + multicdns:
+                    if cdn.lower() in domain['cname_name']:
+                        target_domains.add(domain['query_name'])
+
+            if domain['query_type'] == "A" and \
+                    domain['response_type'] == "A":
+                res[domain['query_name']] = domain["response_ttl"]
+
+    print("Done with {}".format(f))
+
+
+from multiprocessing.dummy import Pool as ThreadPool
+
+if __name__ == "__main__":
+    files = os.listdir('/tmp/')
+
+    files_list = []
+    for f in files:
+        if f.endswith('.avro'):
+            files_list.append(f)
+
+    print("Total files {}".format(len(files_list)))
+
+    pool = ThreadPool(30)
+    results = pool.map(proc_f, files_list)
+    pool.close()
+    pool.join()
+
+
+    json_dump(target_domains, 'target_v2.json')
+    json_dump(res, 'result_v2.json')
