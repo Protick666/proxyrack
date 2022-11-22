bd6759b99d339b49e818c500a31cfc1a1c210cf5
protickbhowmick <protickbhowmick@priyo.com>
2022-11-22 02:11:57 -0500

    init

--- ocsp_simulation/analyzer_fake.py
+++ ocsp_simulation/analyzer_fake.py
@@ -297,6 +297,12 @@ def until_first_filter():
 
     qname_to_rtt_list_vt, qname_to_rtt_list_nvt = build_dns()
 
+    with open('qname_to_rtt_list_vt.json', "w") as ouf:
+        json.dump(qname_to_rtt_list_vt, fp=ouf)
+
+    with open('qname_to_rtt_list_nvt.json', "w") as ouf:
+        json.dump(qname_to_rtt_list_nvt, fp=ouf)
+
     qname_to_median_rtt_vt = get_median_dict(qname_to_rtt_list_vt)
     qname_to_median_rtt_nvt = get_median_dict(qname_to_rtt_list_nvt)
 
@@ -323,6 +329,10 @@ def until_first_filter():
     perc_change = []
     perc_change_cache = []
 
+    not_found_server_dns = set()
+    not_found_host_dns = set()
+    not_found_host_ocsp = set()
+
     for e in list_only_first_filter:
         #         fields = ['client_hello_time', 'server_hello_time', 'change_cipher_time_server', 'server_name', 'fingerprint']
         server_name = e['server_name']
@@ -334,17 +344,27 @@ def until_first_filter():
         elif server_name in qname_to_median_rtt_nvt:
             dns_A_time = qname_to_median_rtt_nvt[server_name]
 
+        if dns_A_time is None:
+            not_found_server_dns.add(dns_A_time)
+
         dns_OCSP_time = None
         if ocsp_host in qname_to_median_rtt_vt:
             dns_OCSP_time = qname_to_median_rtt_vt[ocsp_host]
         elif ocsp_host in qname_to_median_rtt_nvt:
             dns_OCSP_time = qname_to_median_rtt_nvt[ocsp_host]
 
+        if dns_OCSP_time is None:
+            not_found_host_dns.add(dns_OCSP_time)
+
         ocsp_http_time = None
         if ocsp_host in host_to_median_http_time:
             ocsp_http_time = host_to_median_http_time[ocsp_host]
 
+        if ocsp_http_time is None:
+            not_found_host_ocsp.add(ocsp_http_time)
+
         if dns_A_time and dns_OCSP_time and ocsp_http_time:
+            print(server_name, ocsp_host)
             client_hello_time = e['client_hello_time']
             server_hello_time = e['server_hello_time'] - client_hello_time + dns_A_time
             change_cipher_time_server = e['change_cipher_time_server'] - client_hello_time + dns_A_time
@@ -372,6 +392,13 @@ def until_first_filter():
 
     with open('perc_change.json', "w") as ouf:
         json.dump(perc_change, fp=ouf)
+
+    with open('not_found_host_ocsp.json', "w") as ouf:
+        json.dump(list(not_found_host_ocsp), fp=ouf)
+    with open('not_found_host_dns.json', "w") as ouf:
+        json.dump(list(not_found_host_dns), fp=ouf)
+    with open('not_found_server_dns.json', "w") as ouf:
+        json.dump(list(not_found_server_dns), fp=ouf)
 
     for e in list_only_first_filter:
         #         fields = ['client_hello_time', 'server_hello_time', 'change_cipher_time_server', 'server_name', 'fingerprint']
