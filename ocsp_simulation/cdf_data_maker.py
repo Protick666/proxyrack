c20795e733fa9ef333a44d30386f7a20707108d1
protickbhowmick <protickbhowmick@priyo.com>
2023-02-14 21:34:48 -0500

    init

--- ocsp_simulation/cdf_data_maker.py
+++ ocsp_simulation/cdf_data_maker.py
@@ -22,8 +22,8 @@ def analyze_single_entry(e):
         # dns_start, dns_end, client_hello_time, server_hello_time, change_cipher_time_client, change_cipher_time_server, established_time, encrypted_data_time_app, ocsp_dns_1, ocsp_dns_2, ocsp_1, ocsp_2, server_name, meta, _ = e
         dns_start, dns_end, client_hello_time, server_hello_time, change_cipher_time_client, change_cipher_time_server, established_time, encrypted_data_time_app, ocsp_dns_1, ocsp_dns_2, ocsp_1, ocsp_2, server_name = e
 
-        if "demdex" in server_name or "mozilla" in server_name:
-            return None, None
+        # if "demdex" in server_name or "mozilla" in server_name:
+        #     return None, None
 
         old_ocsp_end = ocsp_2
         reactive_ocsp_end = max(ocsp_dns_2, established_time)
@@ -57,51 +57,59 @@ def analyze_zeek_output(file):
     return arr_reactive, arr_proactive
 
 
-store_dict = {}
+def analyze_init():
+    store_dict = {}
 
-for mode in modes:
-    for staple_mode in staple_modes:
-        first_reactive_arr = []
-        first_proactive_arr = []
-        second_proactive_arr = []
-        second_reactive_arr = []
+    for mode in modes:
+        for staple_mode in staple_modes:
+            first_reactive_arr = []
+            first_proactive_arr = []
+            second_proactive_arr = []
+            second_reactive_arr = []
 
-        first_domains = 0
-        second_domains = 0
+            first_domains = 0
+            second_domains = 0
+
+            files = get_leaf_files("{}/{}/{}".format(result_path, mode, staple_mode))
+            for file in files:
+                segments = file.split("/")
+                file_name = segments[-1]
+                sub_segs = file_name.split("-")
+                index_first = int(sub_segs[0])
+                index_second = int(sub_segs[1][: -5])
+                arr_reactive, arr_proactive = analyze_zeek_output(file)
+
+                if index_first < 30000:
+                    first_domains += index_second - index_first + 1
+                    first_reactive_arr += arr_reactive
+                    first_proactive_arr += arr_proactive
+                else:
+                    second_domains += index_second - index_first + 1
+                    second_reactive_arr += arr_reactive
+                    second_proactive_arr += arr_proactive
+
+            mother_str = "{}-{}".format(mode, staple_mode)
+            store = {}
+            store["first_reactive_arr"] = first_reactive_arr
+            store["second_reactive_arr"] = second_reactive_arr
+            store["first_proactive_arr"] = first_proactive_arr
+            store["second_proactive_arr"] = second_proactive_arr
+
+            store["f_domains"] = first_domains
+            store["l_domains"] = second_domains
+
+            store_dict[mother_str] = store
+
+    with open("mother_dict.json", "w") as ouf:
+        json.dump(store_dict, fp=ouf)
 
 
-        files = get_leaf_files("{}/{}/{}".format(result_path, mode, staple_mode))
-        for file in files:
-            segments = file.split("/")
-            file_name = segments[-1]
-            sub_segs = file_name.split("-")
-            index_first = int(sub_segs[0])
-            index_second = int(sub_segs[1][: -5])
-            arr_reactive, arr_proactive = analyze_zeek_output(file)
+def analyze_second_step():
+    f = open("data/mother_dict.json")
+    d = json.load(f)
+    a = 1
 
-            if index_first < 30000:
-                first_domains += index_second - index_first + 1
-                first_reactive_arr += arr_reactive
-                first_proactive_arr += arr_proactive
-            else:
-                second_domains += index_second - index_first + 1
-                second_reactive_arr += arr_reactive
-                second_proactive_arr += arr_proactive
-
-        mother_str = "{}-{}".format(mode, staple_mode)
-        store = {}
-        store["first_reactive_arr"] = first_reactive_arr
-        store["second_reactive_arr"] = second_reactive_arr
-        store["first_proactive_arr"] = first_proactive_arr
-        store["second_proactive_arr"] = second_proactive_arr
-
-        store["f_domains"] = first_domains
-        store["l_domains"] = second_domains
-
-        store_dict[mother_str] = store
-
-with open("mother_dict.json", "w") as ouf:
-    json.dump(store_dict, fp=ouf)
+analyze_second_step()
 
 
 
