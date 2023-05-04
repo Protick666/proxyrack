a317da1c8f1dc3bfd94b0f00a588a80f218c4135
protickbhowmick <protickbhowmick@priyo.com>
2023-05-03 22:38:30 -0400

    aas

--- ocsp-luminati/luminati.py
+++ ocsp-luminati/luminati.py
@@ -1,4 +1,9 @@
+import time
 from multiprocessing.dummy import Pool as ThreadPool
+
+
+
+
 def get_files_from_dir(path):
     from os import listdir
     from os.path import isfile, join
@@ -24,7 +29,11 @@ def find_response_time(s):
 
 from collections import defaultdict
 
+ocsp_url_to_err_list = defaultdict(lambda : list())
+
 ocsp_url_to_tuple_list = defaultdict(lambda : list())
+
+ocsp_url_counter = defaultdict(lambda : 0)
 
 files = get_files_from_dir('/net/data/luminati/no-nonce/post')
 index = 0
@@ -33,7 +42,28 @@ for file in files:
     files_numbered.append((file, "{}/{}".format(index, len(files))))
     index += 1
 
+'''
+    Meta:
+
+    Number of OCSP URL
+    Number of req sent
+    Number of distinct serials
+    Number of ASNs
+
+    Number of req sent succ
+    Number of req sent unsucc
+
+'''
+
+ocsp_url_set = set()
+num_req = 0
+serial_set = set()
+asn_set = set()
+succ_req = 0
+unsucc_req = 0
+
 def analyze_single_file(tuple):
+    global num_req, succ_req, unsucc_req, ocsp_url_set, serial_set, asn_set
     try:
         file, index = tuple
         import json
@@ -41,11 +71,33 @@ def analyze_single_file(tuple):
         g = json.load(f)
         for d in g:
             try:
+                num_req += 1
+                is_normal = d['is_normal']
                 url = d['target']
+
+                ocsp_url_set.add(url)
+
+                asn = d['hop']
                 serial = d['serial_number']
+                pre_time = d['time-pre']
+
+                serial_set.add(serial)
+                asn_set.add(asn)
+
+                ocsp_url_counter[url] += 1
+
+                if not is_normal:
+                    unsucc_req += 1
+                    err_reason = d['error']
+                    ocsp_url_to_err_list[url].append(
+                        (asn, serial, err_reason, pre_time))
+                    continue
+
+                succ_req += 1
+
                 time_start = d['time-start']
                 time_end = d['time-end']
-                asn = d['hop']
+
                 ocsp_response_status = d['ocsp_response_status']
                 delegated_response = d['delegated_response']
                 ocsp_cert_status = d['ocsp_cert_status']
@@ -55,11 +107,12 @@ def analyze_single_file(tuple):
                 if ocsp_response_status == "OCSPResponseStatus.SUCCESSFUL" and is_normal:
                     ocsp_url_to_tuple_list[url].append(
                         (asn, serial, delegated_response, time_end - time_start, response_time_lum))
-            except:
-                pass
+
+            except Exception as e:
+                print(e)
         print("Done with {}".format(index))
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
 
     # "ocsp_response_status": "OCSPResponseStatus.SUCCESSFUL",
@@ -71,11 +124,34 @@ print("Total files {}".format(len(files_numbered)))
 
 files_numbered = files_numbered[: 100]
 
+t1 = time.time()
 pool = ThreadPool(100)
 results = pool.map(analyze_single_file, files_numbered)
 pool.close()
 pool.join()
+t2 = time.time()
+print("Total time taken {} minutes".format((t2 - t1) / 60))
 
 import json
-with open("lum_summary.json", "w") as ouf:
+with open("lum_data/lum_summary.json", "w") as ouf:
     json.dump(ocsp_url_to_tuple_list, fp=ouf)
+
+
+summary = {
+    "total_ocsp_url": len(ocsp_url_set),
+    "total req sent": num_req,
+    "succ": succ_req,
+    "unsucc": unsucc_req,
+    "total serials": len(serial_set),
+    "total asn": len(asn_set)
+}
+
+with open("lum_data/lum_err_summary.json", "w") as ouf:
+    json.dump(ocsp_url_to_err_list, fp=ouf)
+
+with open("lum_data/meta_info_lum.json", "w") as ouf:
+    json.dump(summary, fp=ouf, indent=2)
+
+with open("lum_data/conuter.json", "w") as ouf:
+    json.dump(ocsp_url_counter, fp=ouf)
+
